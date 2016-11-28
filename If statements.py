611d92ccd8c8e798cf50a9fd0a2b0d38385ef737
abe <adealba@csumb.edu>
2016-11-28 10:35:17 -0800

    If statements abe, sebastian

---
+++ If statements.py
@@ -0,0 +1,32 @@
+#Left Drumstick
+
+s = Sound()
+s.read('(name of file).wav')
+
+if "drumstickL" > x && "drumstickL" < xx:
+	if "drumstickL" > y && "drumstickL" < yy:
+		s.play()
+if "drumstickL" > x1 && "drumstickL" < xx1:
+	if "drumstickL" > y1 && "drumstickL" < yy1:
+		s.play()
+if "drumstickL" > x2 && "drumstickL" < xx2:
+	if "drumstickL" > y1 && "drumstickL" < yy1:
+		s.play()
+if "drumstickL" > x3 && "drumstickL" < xx3:
+	if "drumstickL" > y && "drumstickL" < yy:
+		s.play()
+
+#Right Drumstick
+
+if "drumstickR" > x && "drumstickR" < xx:
+	if "drumstickL" > y && "drumstickL" < yy:
+		s.play()
+if "drumstickR" > x1 && "drumstickR" < xx1:
+	if "drumstickR" > y1 && "drumstickR" < yy1:
+		s.play()
+if "drumstickR" > x2 && "drumstickR" < xx2:
+	if "drumstickR" > y1 && "drumstickR" < yy1:
+		s.play()
+if "drumstickR" > x3 && "drumstickR" < xx3:
+	if "drumstickR" > y && "drumstickR" < yy:
+		s.play()
